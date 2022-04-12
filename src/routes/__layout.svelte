0e37f3ebcd3d0c6d766bc00cb5f7341ea0e57ab8
mattcroat <mattcroat.dev@gmail.com>
2022-04-12 14:22:33 +0200

    feat: Blur background on scroll

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
+  import { browser } from '$app/env'
+
   import Header from '$root/components/ui/header.svelte'
   import Footer from '$root/components/ui/footer.svelte'
   import '$root/styles/global.css'
+
+  let scrollY = 0
+  const htmlElement = browser && document.documentElement
+
+  $: {
+    if (browser) {
+      if (scrollY > 400) {
+        htmlElement.style.setProperty('--blur-bg', '200px')
+        htmlElement.style.setProperty('--blur-bg-opacity', '60%')
+      } else {
+        htmlElement.style.setProperty('--blur-bg', '0px')
+        htmlElement.style.setProperty('--blur-bg-opacity', '40%')
+      }
+    }
+  }
 </script>
+
+<svelte:window bind:scrollY />
 
 <div class="grid">
   <Header />
