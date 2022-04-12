644ad1555ced3086607b9d79ae815c63a603831d
mattcroat <mattcroat.dev@gmail.com>
2022-04-12 11:43:48 +0200

    feat: Category page

---
+++ src/routes/categories/[category].svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Heading from '$root/components/ui/heading.svelte'
+  import Posts from '$root/components/ui/posts.svelte'
+
+  import { posts } from '$root/data/posts.json'
+</script>
+
+<Heading>Category</Heading>
+<Posts {posts}>
+  <div class="container" slot="title">
+    <div>
+      <span class="tag">Category</span>
+    </div>
+    <div>
+      <span class="results">{posts.length}</span> results
+    </div>
+  </div>
+</Posts>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .tag {
+    padding: var(--spacing-16);
+    font-weight: 700;
+    background-color: var(--clr-bg);
+    border-radius: var(--rounded-20);
+    box-shadow: var(--shadow-sm);
+  }
+
+  .results {
+    font-weight: 700;
+  }
+</style>
