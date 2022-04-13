6c124ca40396b00ab38fa5f61c6258691d65b1f1
mattcroat <mattcroat.dev@gmail.com>
2022-04-13 17:24:05 +0200

    chore: Add container, set SVG fill to none

--- src/components/preferences/index.svelte
+++ src/components/preferences/index.svelte
@@ -13,55 +13,57 @@
   import Reset from './reset.svelte'
 </script>
 
-<Popover let:open class="popover">
-  <PopoverButton>
-    <CogIcon width="24" height="24" />
-  </PopoverButton>
+<div class="container">
+  <Popover let:open class="popover">
+    <PopoverButton>
+      <CogIcon width="24" height="24" />
+    </PopoverButton>
 
-  {#if open}
-    <div transition:fade={{ duration: 100 }}>
-      <PopoverPanel class="popover-panel" static>
-        <div class="preferences">
-          <svg
-            width="24"
-            height="24"
-            class="arrow"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              id="inside"
-              d="M23 24H1L11.0909 1.98341C11.4474 1.20562 12.5526 1.20562 12.9091 1.98341L23 24Z"
-              fill="#0066FF"
-            />
-            <path
-              id="outside"
-              d="M12.8944 1.78885L24 24H23L12.9021 2.88628C12.5396 2.12822 11.4604 2.12822 11.0979 2.88628L1 24H0L11.1056 1.78885C11.4741 1.05181 12.5259 1.0518 12.8944 1.78885Z"
-              fill="#FF0000"
-            />
-          </svg>
+    {#if open}
+      <div transition:fade={{ duration: 100 }}>
+        <PopoverPanel class="popover-panel" static>
+          <div class="preferences">
+            <svg
+              width="24"
+              height="24"
+              class="arrow"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                id="inside"
+                d="M23 24H1L11.0909 1.98341C11.4474 1.20562 12.5526 1.20562 12.9091 1.98341L23 24Z"
+                fill="none"
+              />
+              <path
+                id="outside"
+                d="M12.8944 1.78885L24 24H23L12.9021 2.88628C12.5396 2.12822 11.4604 2.12822 11.0979 2.88628L1 24H0L11.1056 1.78885C11.4741 1.05181 12.5259 1.0518 12.8944 1.78885Z"
+                fill="none"
+              />
+            </svg>
 
-          <span class="title">Preferences</span>
+            <span class="title">Preferences</span>
 
-          <div class="options">
-            <Themes />
-            <Reading />
-            <Dyslexic />
-            <Reset />
+            <div class="options">
+              <Themes />
+              <Reading />
+              <Dyslexic />
+              <Reset />
+            </div>
           </div>
-        </div>
-      </PopoverPanel>
-    </div>
-  {/if}
-</Popover>
+        </PopoverPanel>
+      </div>
+    {/if}
+  </Popover>
+</div>
 
 <style>
-  :global(.popover) {
+  .container :global(.popover) {
     position: relative;
   }
 
-  :global(.popover-panel) {
+  .container :global(.popover-panel) {
     position: absolute;
     top: 48px;
     right: -22px;
