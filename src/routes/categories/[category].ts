c69f1947b0e67b6b5b74e00fd74aedfdefc60920
mattcroat <mattcroat.dev@gmail.com>
2022-04-21 21:23:29 +0200

    feat: Error page

--- src/routes/categories/[category].ts
+++ src/routes/categories/[category].ts
@@ -1,8 +1,15 @@
 import type { RequestHandler } from '@sveltejs/kit'
 
 import { getPostsByCategory } from '$root/lib/posts'
+import { categories } from '$root/lib/config'
 
 export const get: RequestHandler = async ({ params }) => {
+  if (!categories[params.category]) {
+    return {
+      status: 404
+    }
+  }
+
   const posts = await getPostsByCategory(params.category)
 
   return {
