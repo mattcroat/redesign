d1cec1d5cf755d8df531c00b81fb24afaff8bdb6
mattcroat <mattcroat.dev@gmail.com>
2022-04-26 14:16:15 +0200

    chore: Update `Cache-Control` header

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,8 +9,10 @@ export const get: RequestHandler = async () => {
     status: 200,
     body: { posts },
     headers: {
-      // 'Cache-Control': `max-age=60, s-maxage=${60}, ETag=${etag}, If-None-Match=${etag}`
-      'Cache-Control': `max-age=60, s-maxage=${60}`
+      // 'Cache-Control': `max-age=600, s-maxage=${60}`,
+      // ETag: etag,
+      // 'If-None-Match': etag
+      'Cache-Control': `max-age=0, s-maxage=${60}`
     }
   }
 }
