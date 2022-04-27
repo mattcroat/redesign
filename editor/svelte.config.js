eaa815a8acbcc7e071b3085fe7acd3547819dba4
mattcroat <mattcroat.dev@gmail.com>
2022-04-27 17:40:05 +0200

    feat: Add custom server for WebSockets

--- editor/svelte.config.js
+++ editor/svelte.config.js
@@ -1,67 +1,8 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 import path from 'path'
-import { Server } from 'socket.io'
 
-import { unified } from 'unified'
-import fromMarkdown from 'remark-parse'
-import fromMarkdownToHtml from 'remark-rehype'
-import parseHtmlAndMarkdown from 'rehype-raw'
-import toHtml from 'rehype-stringify'
-import matter from 'gray-matter'
-
-import remarkGfm from 'remark-gfm'
-import remarkHeadings from 'remark-autolink-headings'
-import remarkSlug from 'remark-slug'
-import remarkSmartypants from 'remark-smartypants'
-import remarkTableofContents from 'remark-toc'
-import rehypePrism from 'rehype-prism-plus'
-import rehypeCodeTitles from 'rehype-code-titles'
-
-async function markdownToHTML(markdown) {
-	const { content } = matter(markdown)
-
-	try {
-		const result = await unified()
-			.use(fromMarkdown)
-			.use([
-				remarkGfm,
-				remarkHeadings,
-				remarkSlug,
-				remarkSmartypants,
-				[remarkTableofContents, { tight: true }]
-			])
-			.use(fromMarkdownToHtml, { allowDangerousHtml: true })
-			.use(rehypeCodeTitles)
-			.use(rehypePrism)
-			.use(parseHtmlAndMarkdown)
-			.use(toHtml)
-			.process(content)
-		const processedMarkdown = result.value
-		return processedMarkdown
-	} catch (error) {
-		if (error instanceof Error) {
-			console.error(error.message)
-			return
-		}
-	}
-}
-
-const webSocketServer = {
-	name: 'webSocketServer',
-	configureServer(server) {
-		const io = new Server(server.httpServer)
-
-		io.on('connection', (socket) => {
-			// receive a message from the client
-			socket.on('updatePreview', async (markdown) => {
-				const html = await markdownToHTML(markdown)
-				// send message to the client
-				socket.emit('previewUpdate', html)
-			})
-		})
-	}
-}
+import { webSocketServer } from './socket/plugin.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
